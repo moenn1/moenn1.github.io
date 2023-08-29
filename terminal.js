74d2350540da4d0594148ec55f8d7bb8720cc7ad
Mohamed Enn <94119838+moenn1@users.noreply.github.com>
2023-08-29 13:34:14 +0100

    New portfolio

---
+++ terminal.js
@@ -0,0 +1,191 @@
+$(function() {
+  
+    $('.prompt').html('root@menn1:~# ');
+
+  var term = new Terminal('#input-line .cmdline', '#container output');
+  term.init();
+  
+});
+
+var util = util || {};
+util.toArray = function(list) {
+  return Array.prototype.slice.call(list || [], 0);
+};
+
+var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
+  window.URL = window.URL || window.webkitURL;
+  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
+  var cmdLine_ = document.querySelector(cmdLineContainer);
+  var output_ = document.querySelector(outputContainer);
+
+  const CMDS_ = [
+    'whoami', 'education', 'interests', 'love','contact', 'blog', 'clear', 'help' 
+  ];
+  
+  var fs_ = null;
+  var cwd_ = null;
+  var history_ = [];
+  var histpos_ = 0;
+  var histtemp_ = 0;
+
+  window.addEventListener('click', function(e) {
+    cmdLine_.focus();
+  }, false);
+
+  cmdLine_.addEventListener('click', inputTextClick_, false);
+  cmdLine_.addEventListener('keydown', historyHandler_, false);
+  cmdLine_.addEventListener('keydown', processNewCommand_, false);
+
+  //
+  function inputTextClick_(e) {
+    this.value = this.value;
+  }
+
+  //
+  function historyHandler_(e) {
+    if (history_.length) {
+      if (e.keyCode == 38 || e.keyCode == 40) {
+        if (history_[histpos_]) {
+          history_[histpos_] = this.value;
+        } else {
+          histtemp_ = this.value;
+        }
+      }
+
+      if (e.keyCode == 38) { // up
+        histpos_--;
+        if (histpos_ < 0) {
+          histpos_ = 0;
+        }
+      } else if (e.keyCode == 40) { // down
+        histpos_++;
+        if (histpos_ > history_.length) {
+          histpos_ = history_.length;
+        }
+      }
+
+      if (e.keyCode == 38 || e.keyCode == 40) {
+        this.value = history_[histpos_] ? history_[histpos_] : histtemp_;
+        this.value = this.value; // Sets cursor to end of input.
+      }
+    }
+  }
+
+  //
+  function processNewCommand_(e) {
+
+    if (e.keyCode == 9) { // tab
+      e.preventDefault();
+      // Implement tab suggest.
+    } else if (e.keyCode == 13) { // enter
+      // Save shell history.
+      if (this.value) {
+        history_[history_.length] = this.value;
+        histpos_ = history_.length;
+      }
+
+      // Duplicate current input and append to output section.
+      var line = this.parentNode.parentNode.cloneNode(true);
+      line.removeAttribute('id')
+      line.classList.add('line');
+      var input = line.querySelector('input.cmdline');
+      input.autofocus = false;
+      input.readOnly = true;
+      output_.appendChild(line);
+
+      if (this.value && this.value.trim()) {
+        var args = this.value.split(' ').filter(function(val, i) {
+          return val;
+        });
+        var cmd = args[0].toLowerCase();
+        args = args.splice(1); // Remove cmd from arg list.
+      }
+
+      switch (cmd) {
+        case 'clear':
+          output_.innerHTML = '';
+          this.value = '';
+          return;
+        case 'help':
+          var result = "<h2>Help</h2><p><b>whoami</b>: display all my information.<br><b>education</b>: display all my information about my education.<br><b>interests</b>: display all my interests.<br><b>love</b>: are you curious about my love?<br><b>media</b>: display where I have been mentioned.<br><b>contact</b>: Say hi<br><b>blog</b>: Link of my blog<br><b>clear</b>: clear terminal<br><b>help</b>: display this menu.</p>";
+          output(result);
+          break;
+        case 'education':
+          var result = "<h3>Education</h3><p>Software Engineering Student at Ecole Nationale Des Sciences Appliquées, to be completed in 2024.</p>";
+
+          output(result);
+          break;
+        case 'interests': 
+        var result = "<h3>Interests</h3><p>Gym, Football, Coding, Problem Solving..</p>";
+
+          output(result);
+          break;
+        case 'blog':
+          var result = "<h3>I regulary share my thoughts and work on medium <a href=\"https://medium.com/@mohamed.enn/\">Blog</a></h3>";
+          output(result);
+          break;
+        case 'contact':
+          var result = "<h3>Contact</h3><h4>Email: mohamed.enn2001@gmail.com<br>LinkedIn: Mohamed EN-NASSIBI<br>Portfolio: moenn1.github.io</h4>";
+          output(result);
+          break;
+        case 'whoami':
+          var result = "<h1>Mohamed EN-NASSIBI</h1><p>4th-year software engineering student with a passion for computer science. Fluent in Dutch, Arabic, English, and French. Experienced freelancer in programming, web design, and copywriting. Eager to apply technical knowledge and problem-solving skills to real-world situations. Confident in delivering high-quality results and always looking for new challenges and opportunities to expand the skill set.</p>";
+          output(result);
+          break;
+        case 'love':
+          output("<h3>404...<br>Just hard times for the moment.</h3>");
+          break;
+        default:
+          if (cmd) {
+            output(cmd + ': command not found');
+          }
+      };
+
+      window.scrollTo(0, getDocHeight_());
+      this.value = ''; // Clear/setup line for next input.
+    }
+  }
+
+  //
+  function formatColumns_(entries) {
+    var maxName = entries[0].name;
+    util.toArray(entries).forEach(function(entry, i) {
+      if (entry.name.length > maxName.length) {
+        maxName = entry.name;
+      }
+    });
+
+    var height = entries.length <= 3 ?
+        'height: ' + (entries.length * 15) + 'px;' : '';
+
+    // 12px monospace font yields ~7px screen width.
+    var colWidth = maxName.length * 7;
+
+    return ['<div class="ls-files" style="-webkit-column-width:',
+            colWidth, 'px;', height, '">'];
+  }
+
+  //
+  function output(html) {
+    output_.insertAdjacentHTML('beforeEnd', '<p>' + html + '</p>');
+  }
+
+  // Cross-browser impl to get document's height.
+  function getDocHeight_() {
+    var d = document;
+    return Math.max(
+        Math.max(d.body.scrollHeight, d.documentElement.scrollHeight),
+        Math.max(d.body.offsetHeight, d.documentElement.offsetHeight),
+        Math.max(d.body.clientHeight, d.documentElement.clientHeight)
+    );
+  }
+
+  //
+  return {
+    init: function() {
+      output('<h1>Mohamed EN-NASSIBI</h1><h3>Software Engineer<br><p>Enter "help" for more information.</p><p> -- This website is forked from Ahmed Lekssays<3</p>');
+    },
+    output: output
+  }
+};
