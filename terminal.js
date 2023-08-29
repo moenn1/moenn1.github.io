6ddd56061bfcfe75a179c9319c037cc28f845ea2
Mohamed Enn <94119838+moenn1@users.noreply.github.com>
2023-08-29 13:51:52 +0100

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -108,7 +108,7 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
           this.value = '';
           return;
         case 'help':
-          var result = "<h2>Help</h2><p><b>whoami</b>: display all my information.<br><b>education</b>: display all my information about my education.<br><b>interests</b>: display all my interests.<br><b>love</b>: are you curious about my love?<br><b>contact</b>: Say hi<br><b>blog</b>: Link of my blog<br><b>clear</b>: clear terminal<br><b>help</b>: display this menu.</p>  -- <p> -- This website is forked from Ahmed Lekssays</p>";
+          var result = "<h2>Help</h2><p><b>whoami</b>: display all my information.<br><b>education</b>: display all my information about my education.<br><b>interests</b>: display all my interests.<br><b>love</b>: are you curious about my love?<br><b>contact</b>: Say hi<br><b>blog</b>: Link of my blog<br><b>clear</b>: clear terminal<br><b>help</b>: display this menu.</p> <p> -- This website is forked from Ahmed Lekssays</p>";
           output(result);
           break;
         case 'education':
